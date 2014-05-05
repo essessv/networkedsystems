a1e373088faad3dafe7586dee95899d1066ee40d
Ess Ess Vee <essessv@gmail.com>
2014-05-04 22:15:12 -0700

    Added pelican theme and an about me page

---
+++ content/pages/aboutme.rst
@@ -0,0 +1,14 @@
+===========
+About 
+===========
+:date: 2014-05-01 13:00
+
+I am an engineer interested in all aspects of networked systems - computer networks (incl. software-defined-networks, network virtualization) and distributed systems. Of particular interest is their application in cloud computing and scalable web backend architectures.
+
+This blog is an attempt to put my understanding into words, as I try to delve deeper into the internals of these complex networked systems.
+
+I have also been a big fan of `Werner Vogel’s blog <http://allthingsdistributed.com>`_, especially his *Back-to-Basics Weekend Reading* series. Check it out when you have the time. A section of this blog will be based on that idea. At least once every two weeks, I will pick one of the seminal published research papers that has had a high impact in the area of (distributed systems|computer networking|operating systems|storage), and will publish a writeup based upon my understanding.  
+
+My background:
+I work as a software engineer in Silicon Valley working on a cloud-computing product. 
+
---
+++ pelicanconf.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'SS'
+SITENAME = u'Networked Systems'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+USE_FOLDER_AS_CATEGORY = True
+
+# Plugins
+PLUGIN_PATH = '../pelican-plugins'
+PLUGINS = ['tipue_search']
+
+
+# Theme
+THEME = '../my_theme/pelican-elegant'
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('All Things Distributed', 'http://www.allthingsdistributed.com'),
+          ('Network Static', 'http://networkstatic.net'),
+          ('High Scalability', 'http://highscalability.com'),
+          ('Scaling Systems', 'http://scalingsystems.com'),
+	  ('Network Heresy', 'http://networkheresy.com'),
+          ('Ethereal Mind', 'http://etherealmind.com'),
+          ('Perspectives', 'http://perspectives.mvdirona.com'),
+          ('The Paper Trail', 'http://the-paper-trail.org'),
+          ('Brain Food for Hackers', 'http://duartes.org/gustavo/blog/'),
+          ('Some really good blogs', 'http://highscalability.com/blog/category/blog'))
+
+# Social widget
+SOCIAL = (('GitHub', 'https://github.com/essessv'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
